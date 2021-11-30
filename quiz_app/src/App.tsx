a5add26506d3c4f79985e95e48db36a1ca0723f0
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-12-01 00:15:23 +0100

    Added QuestionCard and fetchQuestions function to interact with API

--- quiz_app/src/App.tsx
+++ quiz_app/src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions, Difficulty } from './API';
 
-function App() {
+const App = () => {
+  const TOTAL_QUESTIONS = 10;
+  const [loading, setLoading ] = useState(false);
+  const [ questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+
+  const startAPIcall = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz App
+      <h1>Quiz App</h1>
+      <button className="start" onClick={startAPIcall}>
+        Start Quiz
+      </button>
+      <p className="score">Score: </p>
+      <p className="loading">Loading Questions ...</p>
+      {/* <QuestionCard 
+        questionNr = {number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
